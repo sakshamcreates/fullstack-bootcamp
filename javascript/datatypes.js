2d7014784371c6f32299bd927043c2ab686305bc
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-08 22:00:48 +0530

    operator in js

---
+++ javascript/datatypes.js
@@ -0,0 +1,21 @@
+const { log } = require("console");
+
+console.log("hello chai");
+
+let score = 102;
+let name = "devsaksham.netlify.PP";
+let isloggedin = false;
+
+
+//object
+
+let teatypes = ["lemon tea","orange tea","oolong tea"];
+
+let user = {firstname:"saksham singh",lastname:"singh"}
+
+let getscore = score;
+
+console.log(teatypes);
+
+console.log(getscore);
+
---
+++ javascript/hello.js
@@ -0,0 +1,5 @@
+console.log("hello.javascript");
+
+console.table({city:"jaipur"})
+
+console.warn({city:"jaipur"})
