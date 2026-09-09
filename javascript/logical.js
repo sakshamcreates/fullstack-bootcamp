05081ab7c89effae1d94c5fff3e0b3a6e8edb0e4
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-09 20:37:10 +0530

    add logical

---
+++ javascript/logical.js
@@ -0,0 +1,16 @@
+// & -> and
+// || -> or
+// ! -> reverse
+
+
+let isloggedin = true
+
+let ispaid = false
+console.log(isloggedin && ispaid) 
+
+
+
+let isemailuser = true
+
+let isgoogleuser = false
+console.log(isemailuser || isgoogleuser);
