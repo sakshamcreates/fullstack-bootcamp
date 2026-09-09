6d14fe18093867bec35b62866cdb73b5dcc2b8bc
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-09 22:10:30 +0530

    add non-primitive

---
+++ javascript/nonprimitive.js
@@ -0,0 +1,23 @@
+let username = {
+    firstname: "saksham",
+    isloggedin:true,
+
+};
+ const myFirstName = "saksham";
+ mYfirstName = "Mr.Saksham";
+username.lastname = "singh"
+ console.log(myFirstName);
+
+ console.log(username["first name"]);
+
+ console.log(username.lastname);
+ console.log(username);
+ console.log(typeof username);
+ 
+
+ let anotherUser = ["saksham","singh",true];
+
+ console.log(anotherUser[0]);
+
+ let isValue = true;
+ console.log(isValue +1);
