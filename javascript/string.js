80b22f915ea7df904d8e2838fa5d7f54c60a49b2
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-09 21:07:55 +0530

    add string

---
+++ javascript/string.js
@@ -0,0 +1,48 @@
+// //number
+
+// let balance = 120;
+
+// let anotherbalance = new Number(120);
+
+// console.log(balance);
+// console.log(anotherbalance);
+
+
+// console.log(typeof balance); 
+
+
+// // console.log(typeof anotherbalance);
+
+
+// //boolean
+
+// let isactive = true
+
+// let isReallyActive = new Boolean(true)//not recommended
+
+// let firstname = null;
+// let lastname = undefined
+// console.log(firstname);
+// console.log(lastname);
+
+//string
+
+let myString = "hello";
+let myStringOne = "hola";
+let username ='saksham';
+
+let oldGreet = myString + "saksham";
+
+let demoOne = 'value is ${2 * 2}';
+//console.log(demoOne);
+
+console.log(oldGreet);
+
+let sm1 = Symbol();
+let sm2 = Symbol();
+
+console.log(sm1 == sm2);
+
+
+
+let greetMassage = 'hello';
